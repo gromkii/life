0fb4ae7374c99f519180ef32df813486d55f288e
Dax <limeside@gmail.com>
2017-01-30 16:29:49 -0600

    Add data structure and init func for grid. :rabbit:

--- life.go
+++ life.go
@@ -4,6 +4,34 @@ import (
   "fmt"
 )
 
+/*
+  We will need a grid to display the game on, comprised of
+  a 2d array of booleans, a width, and a height. True = alive,
+  false = dead.
+*/
+type Grid struct {
+  cell [][]bool
+  width, height int
+}
+
+/*
+  We need a method to initialize a new grid, as well as set each
+  cell to false by default. Returns the Grid type we just created.
+*/
+func NewGrid(width, height) Grid {
+  // Define the 2d array to being looping through.
+  cell := make([][]bool, height)
+  for i:=0 ; i < range cell ; i++ {
+    cell[i] = make([]bool, width)
+    for j:=0; j < range cell[i]; j++ {
+      cell[i][j] = false
+    }
+    
+  }
+
+  return Grid{cell:cell, width:width, height:height}
+}
+
 func main(){
   fmt.Println("Hello from Go!")
 }
